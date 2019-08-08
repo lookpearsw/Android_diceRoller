1a6cb3ce3407db242daf952e8dd7058bbb1cd799
lookpearsw <supajeew@gmail.com>
2019-08-08 16:00:53 +0700

    Change the text: Display a string

--- app/src/main/java/com/gmail/supajeew/diceroller/MainActivity.kt
+++ app/src/main/java/com/gmail/supajeew/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gmail.supajeew.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +17,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
+
+
+
     }
 }
 
